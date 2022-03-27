d5a96e7b7820a932c3f652a2e338149378b12472
neki-dev <dev@neki.guru>
2022-03-27 19:46:20 +0300

    Fixes

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ module.exports = {
   rules: {
     'react/jsx-max-props-per-line': [1, { "maximum": 3 }],
     'react/jsx-props-no-spreading': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/anchor-has-content': 'off',
     'import/extensions': 'off',
     'no-underscore-dangle': 'off',
     '@typescript-eslint/member-delimiter-style': [
