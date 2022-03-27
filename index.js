3c35fae42f23c2a0c444a8357a512fb912a41b11
neki-dev <dev@neki.guru>
2022-03-27 19:12:14 +0300

    Updates

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
-    },
-    parser: "@typescript-eslint/parser",
-    extends: ['airbnb', 'airbnb-typescript'],
-    rules: {
-      '@typescript-eslint/no-throw-literal': 'off',
-      'consistent-return': 'off',
-      'no-restricted-syntax': 'off',
-      'no-underscore-dangle': 'off',
-      'import/extensions': 'off',
-      'import/no-extraneous-dependencies': 'off',
-      'react/jsx-max-props-per-line': [1, { "maximum": 3 }],
-      '@typescript-eslint/member-delimiter-style': [
-        'error',
-        {
-          multiline: {
-            delimiter: 'none',
-            requireLast: true
-          },
-          singleline: {
-            delimiter: 'semi',
-            requireLast: false
-          }
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json"
+  },
+  extends: ['airbnb', 'airbnb-typescript'],
+  rules: {
+    'react/jsx-max-props-per-line': [1, { "maximum": 3 }],
+    'import/extensions': 'off',
+    'no-underscore-dangle': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false
         }
-      ],
-    },
-  };
+      }
+    ],
+  },
+};
