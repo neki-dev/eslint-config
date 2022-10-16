286fbd7a31dab4bb63c63efb23d6c90edc7f6432
neki-dev <dev@neki.guru>
2022-10-16 16:12:11 +0300

    Updated global rules

--- rules/js.js
+++ rules/js.js
@@ -5,4 +5,7 @@ module.exports = {
   "no-underscore-dangle": "off",
   "no-restricted-syntax": "off",
   "no-plusplus": "off",
+  "max-len": ["warn", { "code": 128 }],
+  "newline-after-var": "warn",
+  "newline-before-return": "warn",
 };
